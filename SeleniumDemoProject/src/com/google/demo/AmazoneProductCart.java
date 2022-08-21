2eaad8e15ea5bec077b08711c2cf7624f935f39b
Kaushal Mistry <Kaushal Mistry@DESKTOP-7F37V1G>
2022-08-21 17:25:54 +0530

    Add to cart is done for amazon

--- SeleniumDemoProject/src/com/google/demo/AmazoneProductCart.java
+++ SeleniumDemoProject/src/com/google/demo/AmazoneProductCart.java
@@ -4,54 +4,111 @@ import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class AmazoneProductCart  {
+//Scenario 1 : Add to cart
+//Scenario 2 : Remove from cart
+//Scenario 3 : Add to saveForLater
+//Scenario 4 : Remove from saveToLater
 
-		public static void main(String[] args) {
+public class AmazoneProductCart {
 
-			System.setProperty("webdriver.chrome.driver",
-					"C:/Users/Kaushal Mistry/Downloads/BrowserDriver/chromedriver.exe");
-			
-			//Locators of element
-			String searchTextBox = "//form/div[2]/div/input";
-			String searchIcon = "//form/div[3]/div";
-			String firstResult = "//div[@cel_widget_id='MAIN-SEARCH_RESULTS-4']/descendant::div[4]";
-			String productTitle = "productTitle";
-			String productPriceToPay = "//span[@data-a-color='base']/span[@class='a-offscreen']";
-			
-			//Web driver initialization
-			WebDriver driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-			
-			//tab 1
-			driver.get("https://www.amazon.in/");
-			driver.findElement(By.xpath(searchTextBox)).sendKeys("Iphone XR");
-			driver.findElement(By.xpath(searchIcon)).click();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-			driver.findElement(By.xpath(firstResult)).click();
-			
-			String currentPageId = driver.getWindowHandle();
-			System.out.println("Current page Id : " + currentPageId);
-			
-			Set<String> windowsIds = driver.getWindowHandles();
-			
-			System.out.println("No of tabs opened : " + windowsIds.size());
-			
-			for (String tabIds : windowsIds) {
-				System.out.println("Id's for tabs : " + tabIds);
+	public static void main(String[] args) {
+
+		AmazoneProductCart amazonTest = new AmazoneProductCart();
+		
+		amazonTest.addProductToCartTest();
+		amazonTest.removeProductFromCartTest();
+
+	}
+
+	public WebDriver webDriverInitialization() {
+		
+				System.setProperty("webdriver.chrome.driver",
+						"C:/Users/Kaushal Mistry/Downloads/BrowserDriver/chromedriver.exe");
+
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+				
+				return driver;
+	}
+	 
+	public void addProductToCartTest() {
+		
+		WebDriver driver = webDriverInitialization();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		// Locators of element
+		String searchTextBox = "//form/div[2]/div/input";
+		String searchIcon = "//form/div[3]/div";
+		String firstResult = "//div[@cel_widget_id='MAIN-SEARCH_RESULTS-4']/descendant::div[4]";
+		String productTitle = "productTitle";
+		String productPriceToPay = "//span[@data-a-color='base']/descendant::span[4]";
+		String productDecimalPrice = "//span[@data-a-color='base']/descendant::span[6]";
+		String addToCartButton = "add-to-cart-button";
+		String addToCartSuccesMsg = "//div[@id='attachDisplayAddBaseAlert']/span";
+
+		//Steps
+		driver.get("https://www.amazon.in/");
+		driver.findElement(By.xpath(searchTextBox)).sendKeys("Iphone XR");
+		driver.findElement(By.xpath(searchIcon)).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+		driver.findElement(By.xpath(firstResult)).click();
+
+		String currentPageId = driver.getWindowHandle();
+
+		Set<String> windowsIds = driver.getWindowHandles();
+
+		//Switching to new tab
+		for (String tabIds : windowsIds) {
+			if (!currentPageId.equals(tabIds)) {
+				driver.switchTo().window(tabIds);
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
 			}
-			
-			driver.switchTo();
-			//tab 2
-			String productName = driver.findElement(By.id(productTitle)).getText();
-			System.out.println("Product Name : " + productName);
-			
-			String productPrice = driver.findElement(By.xpath(productPriceToPay)).getText();
-			System.out.println("Product price to pay : " + productPrice);
 		}
+
+		String productName = driver.findElement(By.id(productTitle)).getText();
+		System.out.println("Product Name : " + productName);
+
+		String productPrice = driver.findElement(By.xpath(productPriceToPay)).getText();
+		String decValues = driver.findElement(By.xpath(productDecimalPrice)).getText();
+		System.out.println("Product price to pay : " + productPrice + "." + decValues);
+
+		WebElement addToCartBtn = driver.findElement(By.id(addToCartButton));
+		js.executeScript("arguments[0].scrollIntoView();", addToCartBtn);
+		addToCartBtn.click();
+
+		WebElement sucessMsg = new WebDriverWait(driver, Duration.ofSeconds(100))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(addToCartSuccesMsg)));
+
+		System.out.println("SuccessMsg : " + sucessMsg.getText());
+		driver.quit();
+	}
+
+	public void removeProductFromCartTest() {
+		
+		WebDriver driver = webDriverInitialization();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		//Locators
+		
+		//Steps
+		
+		//driver.quit();
+		
 	}
 	
+	public void addProductToSaveToLaterTest() {
+		
+	}
 	
+	public void removeProductFromSaveToLaterTest() {
+		
+	}
+}
